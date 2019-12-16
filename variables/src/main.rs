f5bcce75d7b5d142cc28bab601d758a048c90e02
Andrew Wei <andrewwei1999@gmail.com>
2019-12-15 18:19:06 -0800

    starting variables, chaptr 3

---
+++ variables/src/main.rs
@@ -0,0 +1,16 @@
+const MAX_POINTS: u32 = 100_000;
+fn main() {
+    let mut x = 5 ;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+    let x = 5;
+    println!("The value of x is: {}", x);
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("The length of spaces: {}", spaces);
+}
