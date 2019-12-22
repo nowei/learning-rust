8ad0595b1a79c8453ca3862b045d104e17e14af8
Andrew Wei <andrewwei1999@gmail.com>
2019-12-22 04:51:23 -0800

    finished chapter 8

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -77,12 +77,67 @@ fn try_strings() {
     println!("{:?}", other);
     println!("{:?}", fin);
 
-    
+    // indexing into strings
+    let s1 = String::from("Здравствуйте");
+    let h = &s1[0..4];
+    println!("{:?}", h);
 
+    for c in "नमस्ते".chars() {
+        println!("{:?}", c);
+    }
+    for b in "नमस्ते".bytes() {
+        println!("{:?}", b);
+    }
 }
 
+use std::collections::HashMap;
+
+fn try_maps() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    println!("{:?}", scores);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let mut scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("{:?}", scores);
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    println!("{:?}", map);
+
+    let mut scores = HashMap::new();
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+    println!("{:?}", score);
+
+    for (key, value) in &scores {
+        println!("{:?}: {:?}", key, value);
+    }
+    scores.insert(String::from("Blue"), 25);
+    scores.insert(String::from("Blue"), 30);
+    println!("{:?}", scores.get(&String::from("Blue")));
+
+    let mut temp = scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
+}
 
 fn main() {
     try_vectors();
     try_strings();
+    try_maps();
 }
