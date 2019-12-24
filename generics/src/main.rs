9d6fa5ca8f50f353f613dfbaa181ce4bd55cef2d
Andrew Wei <andrewwei1999@gmail.com>
2019-12-23 18:29:04 -0800

    finished adding generics to comparison

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,4 +1,20 @@
-fn largest(list: &[i32]) -> i32 {
+mod lib;
+use lib::Tweet;
+use lib::Summary;
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for item in list.iter() {
+        if item > largest {
+            largest = &item;
+        }
+    }
+    return largest;
+}
+
+
+fn largest_alt<T>(list: &[T]) -> T 
+where T: PartialOrd + Copy {
     let mut largest = list[0];
     for &item in list.iter() {
         if item > largest {
@@ -8,12 +24,43 @@ fn largest(list: &[i32]) -> i32 {
     return largest;
 }
 
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
     println!("{:?}", largest(&number_list));
+    println!("{:?}", &number_list);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
     println!("{:?}", largest(&number_list));
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    println!("{:?}", largest(&char_list));
+
+    let integer = Point {x : 5, y : 10};
+    let float = Point {x : 1.0, y : 4.0};
+    println!("{:?}, {:?}", integer, float);
+    println!("{:?}", integer.x);
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false
+    };
+
+    println!("1 new tweet: {:?}", tweet.summarize());
 }
